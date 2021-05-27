487b9972eea429d801471f446cc7d8bf5e8ae331
Brandon Johnson <brandon@moov.io>
2021-05-26 18:06:34 -0600

    consul package with client and config

---
+++ internal/consul/client.go
@@ -0,0 +1,54 @@
+// generated-from:aac4f94179a969295e94b4572607e42b1419ca91e6a2c905c76717dc6a2f2525 DO NOT REMOVE, DO UPDATE
+
+// Licensed to The Moov Authors under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. The Moov Authors licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package consul
+
+import (
+	consul "github.com/hashicorp/consul/api"
+	"github.com/moov-io/base/log"
+)
+
+type Config struct {
+	Address             string
+	Scheme        		string
+	Name				string
+	Tags				[]string
+}
+
+type Client struct {
+	cfg *Config
+	client *consul.Client
+}
+
+func NewConsulClient(logger log.Logger, config *Config) (*Client, error) {
+	// Default settings we approve of
+	consulClient, err := consul.NewClient(&consul.Config{
+		Address: config.Address,
+		Scheme: config.Scheme,
+	})
+
+	if err != nil {
+		return nil, logger.Fatal().LogErrorf("Error connecting to Consul (%s): %v", config.Name, err).Err()
+	}
+
+	return &Client{
+		cfg: config,
+		client: consulClient,
+	}, nil
+}
