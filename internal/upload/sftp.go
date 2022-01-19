ec6c11f5614ae0d59f2798d1dd5bf7cea2a33122
Adam Shannon <adamkshannon@gmail.com>
2022-01-19 13:12:19 -0600

    upload: record SFTP retry attempts

--- internal/upload/sftp.go
+++ internal/upload/sftp.go
@@ -32,6 +32,11 @@ var (
 		Name: "sftp_agent_up",
 		Help: "Status of SFTP agent connection",
 	}, []string{"hostname"})
+
+	sftpConnectionRetries = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Name: "sftp_connection_retries",
+		Help: "Counter of SFTP connection retry attempts",
+	}, []string{"hostname"})
 )
 
 type SFTPTransferAgent struct {
@@ -152,6 +157,9 @@ func sftpConnect(logger log.Logger, cfg service.UploadAgent) (*ssh.Client, io.Wr
 	var err error
 	for i := 0; i < 3; i++ {
 		if client == nil {
+			if i > 0 {
+				sftpConnectionRetries.With("hostname", cfg.SFTP.Hostname).Add(1)
+			}
 			client, err = ssh.Dial("tcp", cfg.SFTP.Hostname, conf) // retry connection
 			time.Sleep(250 * time.Millisecond)
 		}
