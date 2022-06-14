a0f0c55e7b4e837b1b16f89bc91a0ee7075d4a9b
Brandon Johnson <brandon@moov.io>
2022-06-14 16:33:56 -0600

    alerting: change alerter_test to separate tests for each alerter type

--- internal/alerting/alerter_test.go
+++ internal/alerting/alerter_test.go
@@ -9,59 +9,71 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewAlerters(t *testing.T) {
-	if os.Getenv("PD_API_KEY") == "" && os.Getenv("SLACK_ACCESS_TOKEN") == "" {
-		t.Skip("Skip TestNewAlerters as PD_API_KEY and SLACK_ACCESS_TOKEN are not set")
+func TestNewAlertersPagerDuty(t *testing.T) {
+	if os.Getenv("PD_API_KEY") == "" {
+		t.Skip("Skip TestNewAlertersPagerDuty as PD_API_KEY is not set")
 	}
 	var cfg service.ErrorAlerting
 	var alerters []Alerter
 	var err error
 
-	if os.Getenv("PD_API_KEY") != "" {
-		cfg = service.ErrorAlerting{
-			PagerDuty: &service.PagerDutyAlerting{
-				ApiKey:     os.Getenv("PD_API_KEY"),
-				RoutingKey: os.Getenv("PD_ROUTING_KEY"),
-			},
-		}
-
-		alerters, err = NewAlerters(cfg)
-		require.NoError(t, err)
-		require.Len(t, alerters, 1)
+	cfg = service.ErrorAlerting{
+		PagerDuty: &service.PagerDutyAlerting{
+			ApiKey:     os.Getenv("PD_API_KEY"),
+			RoutingKey: os.Getenv("PD_ROUTING_KEY"),
+		},
 	}
 
-	if os.Getenv("SLACK_ACCESS_TOKEN") != "" {
-		cfg = service.ErrorAlerting{
-			Slack: &service.SlackAlerting{
-				AccessToken: os.Getenv("SLACK_ACCESS_TOKEN"),
-				ChannelID:   os.Getenv("SLACK_CHANNEL_ID"),
-			},
-		}
+	alerters, err = NewAlerters(cfg)
+	require.NoError(t, err)
+	require.Len(t, alerters, 1)
+}
 
-		alerters, err = NewAlerters(cfg)
-		require.NoError(t, err)
-		require.Len(t, alerters, 1)
+func TestNewAlertersSlack(t *testing.T) {
+	if os.Getenv("SLACK_ACCESS_TOKEN") == "" {
+		t.Skip("Skip TestNewAlertersSlack as SLACK_ACCESS_TOKEN is not set")
+	}
+	var cfg service.ErrorAlerting
+	var alerters []Alerter
+	var err error
+
+	cfg = service.ErrorAlerting{
+		Slack: &service.SlackAlerting{
+			AccessToken: os.Getenv("SLACK_ACCESS_TOKEN"),
+			ChannelID:   os.Getenv("SLACK_CHANNEL_ID"),
+		},
 	}
 
-	if os.Getenv("PD_API_KEY") != "" && os.Getenv("SLACK_ACCESS_TOKEN") != "" {
-		cfg = service.ErrorAlerting{
-			PagerDuty: &service.PagerDutyAlerting{
-				ApiKey:     os.Getenv("PD_API_KEY"),
-				RoutingKey: os.Getenv("PD_ROUTING_KEY"),
-			},
-			Slack: &service.SlackAlerting{
-				AccessToken: os.Getenv("SLACK_ACCESS_TOKEN"),
-				ChannelID:   os.Getenv("SLACK_CHANNEL_ID"),
-			},
-		}
+	alerters, err = NewAlerters(cfg)
+	require.NoError(t, err)
+	require.Len(t, alerters, 1)
+}
 
-		alerters, err = NewAlerters(cfg)
+func TestNewAlertersPagerDutyAndSlack(t *testing.T) {
+	if os.Getenv("PD_API_KEY") == "" && os.Getenv("SLACK_ACCESS_TOKEN") == "" {
+		t.Skip("Skip as PD_API_KEY and SLACK_ACCESS_TOKEN are not set")
+	}
+	var cfg service.ErrorAlerting
+	var alerters []Alerter
+	var err error
+
+	cfg = service.ErrorAlerting{
+		PagerDuty: &service.PagerDutyAlerting{
+			ApiKey:     os.Getenv("PD_API_KEY"),
+			RoutingKey: os.Getenv("PD_ROUTING_KEY"),
+		},
+		Slack: &service.SlackAlerting{
+			AccessToken: os.Getenv("SLACK_ACCESS_TOKEN"),
+			ChannelID:   os.Getenv("SLACK_CHANNEL_ID"),
+		},
+	}
+
+	alerters, err = NewAlerters(cfg)
+	require.NoError(t, err)
+	require.Len(t, alerters, 2)
+
+	for _, alerter := range alerters {
+		err = alerter.AlertError(errors.New("error message"))
 		require.NoError(t, err)
-		require.Len(t, alerters, 2)
-
-		for _, alerter := range alerters {
-			err = alerter.AlertError(errors.New("error message"))
-			require.NoError(t, err)
-		}
 	}
 }
